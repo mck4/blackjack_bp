531465dc2dc2cf06a77730a20a55e2871955707f
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-10 15:59:18 -0500

    Added some more test

--- start.py
+++ start.py
@@ -1,7 +1,18 @@
 # Can you see this?
-# I was thinking maybe it would be ideal to separate the bp, the main(start.py, potentially),
-# and blackjack.py for the simulator. I'm not sure if we would need two bps for the two and three
-# card hand? I don't think so but I didn't make it yet for this reason.
+# I was thinking maybe it would be ideal to separate the bp, the main(start.py),
+# and blackjack.py for the simulator, you know, like Dr. O'Hara did it. I don't think
+# Python gives you too difficult of a time doing that.
+
+# I'm not sure if we would need two bps for the two and three # card hand?
+# I don't think so but I didn't make it yet for this reason.
+# By the way if you want to make separate .py files for the classes, feel free.
+
+#If you want it to show up on github, make sure you commit and push. I was just committing &
+#it wasn't showing up but when I pushed, it showed up
+
+# So, in order:
+# 1) VCS>Commit...
+# 2) VCS>Git>Push...
 
 
 # For starting the thing
