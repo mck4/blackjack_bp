ea39ca36fa069eac79d4a8d5f37e52abd9d727a3
mck4 <35850175+mck4@users.noreply.github.com>
2018-10-10 15:44:16 -0500

    Add files via upload
    try :(

---
+++ start.py
@@ -0,0 +1,8 @@
+# Can you see this?
+# I was thinking maybe it would be ideal to separate the bp, the main(start.py, potentially),
+# and blackjack.py for the simulator. I'm not sure if we would need two bps for the two and three
+# card hand? I don't think so but I didn't make it yet for this reason.
+
+# For starting the thing
+def do_it():
+    pass
