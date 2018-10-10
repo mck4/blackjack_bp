a0f9a9cc5ad53b875325dd5740e8b0abf5fb30f7
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-10 15:51:04 -0500

    try

--- start.py
+++ start.py
@@ -3,8 +3,8 @@
 # and blackjack.py for the simulator. I'm not sure if we would need two bps for the two and three
 # card hand? I don't think so but I didn't make it yet for this reason.
 
+
 # For starting the thing
 def do_it():
     pass
 
-#trying to commit
