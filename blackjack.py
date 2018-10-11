1a39c0df67be8e68c8b2fbd41295fd66be78c72b
Carolina Kimbrough <carolinamargaritak@gmail.com>
2018-10-11 15:30:19 -0500

    added a bunch of stuff

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,7 @@
+import start
 # This file is empty!
+
+
+def func(variable):
+    print("hello")
+    print(variable)
--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+#import blackjack
 # Can you see this?
 # I was thinking maybe it would be ideal to separate the bp, the main(start.py),
 # and blackjack.py for the simulator, you know, like Dr. O'Hara did it. I don't think
@@ -16,7 +17,104 @@
 
 #TEST
 
-# For starting the thing
-def do_it():
-    pass
+import random
 
+# Returns a random weight to five decimals places
+def randWeight():
+    random_weight = round(random.uniform(-1.0, 1.0), 5)
+    return random_weight
+
+class backProp:
+    inputs = None
+    hiddens = None
+    outputs = None
+    eta = None
+    weightBottoms = []
+    weightTops = []
+    biasBottom = []
+    biasTop = []
+    hidden = []
+    output = []
+
+    def __init__(self, inputs, hiddens, outputs, eta):
+        self.inputs = inputs
+        self.hiddens = hiddens
+        self.outputs = outputs
+        self.eta = eta
+
+        for i in range(self.inputs):
+            column = []
+            for j in range(self.hiddens):
+                column.append(randWeight())
+            self.weightBottoms.append(column)
+
+
+        for i in range(self.hiddens):
+            column = []
+            for j in range(self.outputs):
+                column.append(randWeight())
+            self.weightTops.append(column)
+
+        for i in range(self.hiddens):
+            self.biasBottom.append(randWeight())
+
+        for i in range(self.outputs):
+            self.biasTop.append(randWeight())
+
+    def get_inputs(self):
+        return self.inputs
+
+    def get_hiddens(self):
+        return self.hiddens
+
+    def get_outputs(self):
+        return self.outputs
+
+    def get_eta(self):
+        return self.eta
+
+    def get_weightBottoms(self):
+        return self.weightBottoms
+
+    def get_weightTops(self):
+        return self.weightTops
+
+    def get_biasBottom(self):
+        return self.biasBottom
+
+    def get_biasTop(self):
+        return self.biasTop
+
+def doit(epochs, showFrequency):
+    backprop = backProp(2, 10, 2, 0.1)
+    #print(backprop.get_weightBottoms())
+
+    rows = len(backprop.get_weightBottoms())
+    cols = 0
+    if rows:
+        cols = len(backprop.get_weightBottoms()[0])
+    for j in range(rows):
+        for i in range(cols):
+                print(str(backprop.get_weightBottoms()[j][i]), "", end="")
+        print()
+
+    print()
+    rows = len(backprop.get_weightTops())
+    cols = 0
+    if rows:
+        cols = len(backprop.get_weightTops()[0])
+    for j in range(rows):
+        for i in range(cols):
+            print(str(backprop.get_weightTops()[j][i]), "", end="")
+        print()
+
+    print()
+    for i in backprop.biasBottom:
+        print(i)
+
+
+
+##############START################
+
+doit(1000000, 100000)
+#print(randWeight())
